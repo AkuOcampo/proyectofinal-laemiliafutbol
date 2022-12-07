f8f5c438ddf4a92f8ae3e4357c368b080f829408
Aku Ocampo <akuocampo@gmail.com>
2022-12-07 15:42:19 -0300

    modifica el index, style y javascript

--- js/script.js
+++ js/script.js
@@ -1,15 +1,43 @@
-let abrir= document.getElementById('abrir');
-let cerrar= document.getElementById('cerrar');
-let popup= document.getElementById('popup');
+/* let edad=confirm("Eres mayor de 18?");
+if (edad==false) {
+	alert("Entras bajo tu responsabilidad")
+}; */
 
-abrir.addEventListener("click", ()=>{
-    popup.classList.add('mostrar');
+let abrir1= document.getElementById('abrir-popup1');
+let cerrar1= document.getElementById('cerrar-popup1');
+let popup1= document.getElementById('popup1');
+
+abrir1.addEventListener("click", ()=>{
+    popup1.classList.add('mostrar');
 });
 
-cerrar.addEventListener("click", ()=>{
-    popup.classList.remove('mostrar');
+cerrar1.addEventListener("click", ()=>{
+    popup1.classList.remove('mostrar');
 });
 
+let abrir2= document.getElementById('abrir-popup2');
+let cerrar2= document.getElementById('cerrar-popup2');
+let popup2= document.getElementById('popup2');
+
+abrir2.addEventListener("click", ()=>{
+    popup2.classList.add('mostrar');
+});
+
+cerrar2.addEventListener("click", ()=>{
+    popup2.classList.remove('mostrar');
+});
+
+let abrir3= document.getElementById('abrir-popup3');
+let cerrar3= document.getElementById('cerrar-popup3');
+let popup3= document.getElementById('popup3');
+
+abrir3.addEventListener("click", ()=>{
+    popup3.classList.add('mostrar');
+});
+
+cerrar3.addEventListener("click", ()=>{
+    popup3.classList.remove('mostrar');
+});
 //-------------------------------------------------------------------
 let div = document.querySelector(".fecha2");
 
