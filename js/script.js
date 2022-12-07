f2053b21d54c00bc2866d189bea2f4532a819ed3
Aku Ocampo <akuocampo@gmail.com>
2022-12-06 22:23:55 -0300

    intento de carrousel de card

--- js/script.js
+++ js/script.js
@@ -19,3 +19,31 @@ document.querySelector(".boton-fecha2").addEventListener("click", () => {
 /* document.querySelector(".show").addEventListener("click", () => {
     div.classList.remove("div_hide");
 }); */
+
+let swiper = new mySwiper('.swiper-container', {
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev'
+	},
+	slidesPerView: 1,
+	spaceBetween: 10,
+	// init: false,
+	breakpoints: {
+	  620: {
+		slidesPerView: 1,
+		spaceBetween: 20,
+	  },
+	  680: {
+		slidesPerView: 2,
+		spaceBetween: 40,
+	  },
+	  920: {
+		slidesPerView: 3,
+		spaceBetween: 40,
+	  },
+	  1240: {
+		slidesPerView: 4,
+		spaceBetween: 50,
+	  },
+	} 
+    });
